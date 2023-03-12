5f5b586bbae967a95848f1b7e8318c0009fcec89
Nathan Nette <nathan.nette@gmail.com>
2023-03-12 22:26:53 +1100

    Have set up revenue aspect and calculation script has basic calc from text.

--- UnityFiles/CommCalc/Assets/Scripts/Calculation.cs
+++ UnityFiles/CommCalc/Assets/Scripts/Calculation.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Calculation : MonoBehaviour
 {
+    public UnityEngine.UI.Text RevTarget;
+    public UnityEngine.UI.Text RevActual;
+    public UnityEngine.UI.Text RevPercent;
+
+    private float fPercentage = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +19,16 @@ public class Calculation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //float fRevTar = float.Parse(RevTarget.text);
+        //float fRevAct = float.Parse(RevActual.text);
+
+        float fRevTar = 75000f;
+        float fRevAct = 25000f;
+
+        fPercentage = (fRevAct / fRevTar);
+        UnityEngine.Debug.Log(fPercentage);
+
+        // Need to add it putting the percentage into the text to show screen.
     }
 
     
